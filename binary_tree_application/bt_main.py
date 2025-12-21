167d962b1f799c2e6ed36cb0bbe5b610520bcc10
Kristina Cas <m.kristinacas@gmail.com>
2025-12-21 15:06:11 +0800

    feat(bt_main): initial input request for binary tree levels

--- binary_tree_application/bt_main.py
+++ binary_tree_application/bt_main.py
@@ -2,7 +2,23 @@ import tkinter as tk
 
 # for user input of node levels and other inputs
 def user_input():
-    pass
+    levels = input("Preferred Binary Tree Level: ")
+    
+    
+    if levels == 1:
+        print("You have 1 node")
+        
+    elif levels == 2:
+        print("You have 3 nodes")
+
+    elif levels == 3:
+        print("You have 7 nodes")
+    
+    elif levels == 4:
+        print("You have 15 nodes")
+    
+    elif levels == 5:
+        print("You have 31 nodes")        
 
 # class to make nodes
 class Node:
