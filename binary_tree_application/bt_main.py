6dfe07fd018c91ae71441ae861f7df57f65fb061
Ma. Kristina Cas <m.kristinacas@gmail.com>
2025-12-21 09:23:10 +0800

    Feature/binary tree/node class
    * feat(bt_main): imported tkinter library
    
    * feat(bt_main): created Node class
    
    * feat(bt_main): initialized other classes needed for the code

---
+++ binary_tree_application/bt_main.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+class Node:
+    def __init__(self, value):
+        self.n_left = None
+        self.n_val = value
+        self.n_right = None
+
+class Tree:
+    pass
+
+class Traversal:
+    pass
+
+class BTMaker:
+    pass
