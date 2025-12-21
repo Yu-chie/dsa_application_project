c8540ebf7c645a4f61d0a66cb7c19a0005b56404
Kristina Cas <m.kristinacas@gmail.com>
2025-12-21 14:29:44 +0800

    refactor(bt_main): placed comments as guides and added a user input method

--- binary_tree_application/bt_main.py
+++ binary_tree_application/bt_main.py
@@ -1,16 +1,24 @@
 import tkinter as tk
 
+# for user input of node levels and other inputs
+def user_input():
+    pass
+
+# class to make nodes
 class Node:
     def __init__(self, value):
         self.n_left = None
         self.n_val = value
         self.n_right = None
 
+# class to make the tree - connections of nodes
 class Tree:
     pass
 
+# class for determining tree traversals
 class Traversal:
     pass
 
+# class for tkinter implementation
 class BTMaker:
     pass
