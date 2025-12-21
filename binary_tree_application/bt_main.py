cc8a55ff7a4fd9185f73e3749d29f616727712ca
Kristina Cas <m.kristinacas@gmail.com>
2025-12-21 16:09:30 +0800

    feat(bt_main): placed logic for user input

--- binary_tree_application/bt_main.py
+++ binary_tree_application/bt_main.py
@@ -9,7 +9,18 @@ class Node:
 
 # class to make the tree - connections of nodes
 class Tree:
-    pass
+    def __init__(self, levels):
+        if not isinstance(levels, int):                                          # checks if user input is correct
+            raise TypeError("Enter a valid number between 1 to 5 only")
+        if levels < 1 or levels > 5:
+            raise ValueError("Enter a valid number between 1 to 5 only")
+        
+        self.levels = levels                                                     # will get value if correct input
+        
+    # build tree
+        
+        
+    
 
 # class for determining tree traversals
 class Traversal:
@@ -19,5 +30,5 @@ class Traversal:
 class BTMaker:
     pass
 
-if __name__ == "__main__":
-    user_input()
+# if __name__ == "__main__":
+    
