41585cf35e967b883f5acd1e4502f7500e77bee5
Victoria Yuki Ilano <rrainy011306@gmail.com>
2025-12-21 11:25:36 +0800

    Feature/queue/oop parking garage (#8)
    * refactor(parking_queue): Add ParkingGarage class and move state variables
    
    * refactor(parking_queue): convert car arrival logic  into class method
    
    * refactor(parking_queue): convert car departure logic into class methos
    
    * refactor(parking_queue): move parking display logic into class method
    
    * refactor(parking_queue): updated main loop to use ParkingGarage object
    
    * docs(queue_pseudocode): Move pseudocode to a new filee

--- queue_application/parking_queue.py
+++ queue_application/parking_queue.py
@@ -6,22 +6,64 @@ Arrival and Departure values represent
 the order of entry and exit, not time
 """
 
-# ------------------------------
-# DATA TO STORE FOR EACH CAR
-# ------------------------------
-# plate_number          -> string (car identifier)
-# arrival_number        -> integer (order of arrival)
-# departure_number      -> integer or "-" (order of departure)
-# parking_slot          -> integer (assigned parking slot vertically)
+# PARKING GARAGE CLASS DEFINITION
+class ParkingGarage:
+    def __init__(self, max_parking):
+        self.max_parking = max_parking  # max parking size
+        self.queue = []                 # parking queue
+        self.arrival_counter = 1        # counts arrivals
+        self.departure_counter = 1      # counts departures
 
-# INITIALIZE VARIABLES
-max_parking = 10        # max parking size
-queue = []              # parking queue
-arrival_counter = 1     # counts arrivals
-departure_counter = 1   # counts departures
+# OPTION 1: ENQUEUE CAR
+    def car_arrives(self):
+        # If parking is full
+        if len(self.queue) >= self.max_parking:
+            print("Parking Garage is Full")
+        else:
+            plate_number = input("Enter Plate Number: ")
+            car = {
+                'plate_number': plate_number,
+                'arrival_number': self.arrival_counter,
+                'departure_number': "-",
+                'parking_slot': len(self.queue) + 1
+            }
+            self.queue.append(car)       # Enqueue FIFO
+            self.arrival_counter += 1
+            print("Car Parked Successfully!")
 
+# OPTION 2: DEQUEUE CAR
+    def car_departs(self):
+        # If parking is empty
+        if len(self.queue) == 0:
+            print("Parking Garage is Empty")
+        else:
+            # Dequeue car
+            car = self.queue.pop(0)
+            car['departure_number'] = self.departure_counter
+            self.departure_counter += 1
+            print(f"Car with Plate Number {car['plate_number']} Departed Successfully!")
+
+# OPTION 3: DISPLAY PARKING TABLE
+    def display_table(self):
+        # If parking is empty
+        if len(self.queue) == 0:
+            print("Parking Garage is Empty")
+        else:
+            # Display table header
+            # : formatting starts ^ center aligned 15 width of column
+            print("\n{:^10} | {:^10} | {:^10} | {:^10}".format('Plate Number', 'Arrival No.', 'Departure No.', 'Parking Slot'))
+            print("-" * 55)
+            for car in self.queue:
+                print("{:^10} | {:^10} | {:^10} | {:^10}".format(
+                    car['plate_number'],
+                    car['arrival_number'],
+                    car['departure_number'],
+                    car['parking_slot']
+                ))
 
 # MAIN PROGRAM LOOP
+garage = ParkingGarage(max_parking=10)  # Set max parking size
+
 while True:
     # Display Menu Options
     print("\n===== Parking Garage Menu =====")
@@ -30,56 +72,15 @@ while True:
     print("3. Display Parking Table")
     print("4. Exit")
     choice = input("Enter your choice (1-4): ")
-
-# OPTION 1: ENQUEUE CAR
+    
     if choice == '1':
-        # If parking is full
-        if len(queue) >= max_parking:
-            print("Parking Garage is Full")
-        else:
-            plate_number = input("Enter Plate Number: ")
-            car = {
-                'plate_number': plate_number,
-                'arrival_number': arrival_counter,
-                'departure_number': "-",
-                'parking_slot': len(queue) + 1
-            }
-            queue.append(car)       # Enqueue FIFO
-            arrival_counter += 1
-            print("Car Parked Successfully!")
-
-# OPTION 2: DEQUEUE CAR
+        garage.car_arrives()
     elif choice == '2':
-        # If parking is empty
-        if len(queue) == 0:
-            print("Parking Garage is Empty")
-        else:
-            # Dequeue car
-            car = queue.pop(0)
-            car['departure_number'] = departure_counter
-            departure_counter += 1
-            print(f"Car with Plate Number {car['plate_number']} Departed Successfully!")
-
-# OPTION 3: DISPLAY PARKING TABLE
+        garage.car_departs()
     elif choice == '3':
-        # If parking is empty
-        if len(queue) == 0:
-            print("Parking Garage is Empty")
-        else:
-            # Display table header
-            # : formatting starts ^ center aligned 15 width of column
-            print("\n{:^10} | {:^10} | {:^10} | {:^10}".format('Plate Number', 'Arrival No.', 'Departure No.', 'Parking Slot'))
-            print("-" * 55)
-            for car in queue:
-                print("{:^10} | {:^10} | {:^10} | {:^10}".format(
-                    car['plate_number'],
-                    car['arrival_number'],
-                    car['departure_number'],
-                    car['parking_slot']
-                ))
-
-# OPTION 4: EXIT PROGRAM
+        garage.display_table()
     elif choice == '4':
-        print("Exiting Program")
-        break       # END OF PROGRAM
-
+        print("Exiting Parking Garage Simulator. Goodbye!")
+        break
+    else:
+        print("Invalid choice. Please enter a number between 1 and 4.")
