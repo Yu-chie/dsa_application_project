2cc9c9d45012f9cfeb31ae8fb50a4bfe8920cd57
Kristina Cas <m.kristinacas@gmail.com>
2025-12-21 15:27:54 +0800

    feat(bt_main): exception handling for tree level inputs of user

--- binary_tree_application/bt_main.py
+++ binary_tree_application/bt_main.py
@@ -2,24 +2,31 @@ import tkinter as tk
 
 # for user input of node levels and other inputs
 def user_input():
-    levels = input("Preferred Binary Tree Level: ")
-    
-    
-    if levels == 1:
-        print("You have 1 node")
+    try:
+        levels = int(input("Preferred Binary Tree Level (1-5): "))
         
-    elif levels == 2:
-        print("You have 3 nodes")
-
-    elif levels == 3:
-        print("You have 7 nodes")
+        if levels < 1 or levels > 5:
+            raise ValueError
+        
+    except ValueError: 
+        print("Please enter a valid number between 1 to 5.")
     
-    elif levels == 4:
-        print("You have 15 nodes")
-    
-    elif levels == 5:
-        print("You have 31 nodes")        
+    else:
+        if levels == 1:
+            print("You have 1 node")
+        
+        elif levels == 2:
+            print("You have 3 nodes")
 
+        elif levels == 3:
+            print("You have 7 nodes")
+    
+        elif levels == 4:
+            print("You have 15 nodes")
+    
+        elif levels == 5:
+            print("You have 31 nodes")
+            
 # class to make nodes
 class Node:
     def __init__(self, value):
