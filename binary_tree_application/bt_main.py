742c7c36ecfe8797abaff8a7d5460ecf899491c1
Ma. Kristina Cas <m.kristinacas@gmail.com>
2025-12-21 16:55:16 +0800

    Feature/binary tree/tree class
    * refactor(bt_main): placed comments as guides and added a user input method
    
    * feat(bt_main): initial input request for binary tree levels
    
    * feat(bt_main): exception handling for tree level inputs of user
    
    * feat(bt_main): placed code to run code (specified for user input method)
    
    * feat(bt_main): looping for exceptiong handling
    
    * refactor(bt_main): removed user input method for now
    
    * feat(bt_main): placed logic for user input
    
    * feat(bt_main): initial logic code for creating a node and its children
    
    * feat(bt_main): added code for adding child nodes
    
    * refactor(bt_main): simplified child nodes creation and fixed variable

--- binary_tree_application/bt_main.py
+++ binary_tree_application/bt_main.py
@@ -1,16 +1,53 @@
 import tkinter as tk
-
+                
+# class to make nodes
 class Node:
-    def __init__(self, value):
+    def __init__(self, data):
         self.n_left = None
-        self.n_val = value
+        self.n_val = data
         self.n_right = None
 
+# class to make the tree - connections of nodes
 class Tree:
-    pass
+    def __init__(self, levels):
+        if not isinstance(levels, int):                                          # checks if user input is correct
+            raise TypeError("Enter a valid number between 1 to 5 only")
+        if levels < 1 or levels > 5:
+            raise ValueError("Enter a valid number between 1 to 5 only")
+        
+        self.levels = levels                                                     # will get value if correct input
+        
+    # build tree
+    def create_node(self, value):                                     # create node object
+        return Node(value)
+    
+    def node_child(self, node, value):                                # make left and right child
+        if value == ".":
+            value = None
+        
+        if node == None:
+            return self.create_node(value)
+        
+        if node.n_left is None:
+            node.n_left = self.create_node(value)
+        
+        elif node.n_right is None:
+            node.n_right = self.create_node(value)
+            
+        else: 
+            self.node_child(node.n_left, value)
+            self.node_child(node.n_right, value)
+        
+        return node
+        
 
+# class for determining tree traversals
 class Traversal:
     pass
 
+# class for tkinter implementation
 class BTMaker:
     pass
+
+# if __name__ == "__main__":
+    
