650f34cb0aef3f662a55bca22903f6b7c719236d
cnriveraa <rivera.cn18@gmail.com>
2025-12-22 15:11:05 +0800

    Implement move_disks method for TowerOfHanoi class to solve the puzzle recursively

--- recursion_application/hanoi_logic.py
+++ recursion_application/hanoi_logic.py
@@ -16,3 +16,13 @@ class TowerOfHanoi:
         self.stacks = {[] for _ in range(3)}
         self.stacks[0] = [size - i for i in range(size)]                      #largest disk at bottom
         self.states = {[(j for j in self.stacks[i])] for i in range(3)}       #to save initial state
+
+    def move_disks(self, number, stack_from, stack_to):
+        if number == 1: #base case
+            self.stacks[stack_to].append(self.stacks[stack_from].pop())
+            self.states.append([[j for j in self.stacks[i]] for i in range(3)])
+        else: #recursive case
+            aux_stack = 3 - stack_from - stack_to
+            self.move_disks(number - 1, stack_from, aux_stack)
+            self.move_disks(1, stack_from, stack_to)
+            self.move_disks(number - 1, aux_stack, stack_to)
