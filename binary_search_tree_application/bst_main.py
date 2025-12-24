70b2524506be63d75f00b2054d9de67349e450ad
Kristina Cas <m.kristinacas@gmail.com>
2025-12-24 13:54:48 +0800

    feat(bst_main): code for creating class with implementation of binary search tree rules

--- binary_search_tree_application/bst_main.py
+++ binary_search_tree_application/bst_main.py
@@ -8,7 +8,27 @@ class Node:
         self.n_right = None
 
 class BSTree:
-    pass
+    def __init__(self, countnode):
+        if not isinstance(countnode, int):
+            raise TypeError("Enter a valid number between 10 to 30 only")
+        if countnode < 10 or countnode > 30:
+            raise ValueError("Enter a valid number between 10 to 30 only")
+        
+        self.countnode = countnode
+    
+    def create_node(self, value):
+        return Node(value)
+    
+    def node_child(self, node, value):
+        if node == None:
+            return self.create_node(value)
+        
+        if value <= node.n_val:                                    # duplicates will automatically be on the left child (as per sir)
+            node.n_left = self.node_child(node.n_left, value)
+        else:
+            node.n_right = self.node_child(node.n_right, value)
+        
+        return node
 
 class BSTMaker:
     pass
