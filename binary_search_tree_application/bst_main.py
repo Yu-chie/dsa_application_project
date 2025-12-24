fa01e997ff712c342d3a1cc2d10a694dcef9aeee
Ma. Kristina Cas <m.kristinacas@gmail.com>
2025-12-24 14:05:34 +0800

    Feature/bst/binary search code (#19)
    * feat(bst_main): initial class structure for logic and impor of needed libraries
    
    * feat(bst_main): added node class
    
    * feat(bst_main): code for creating class with implementation of binary search tree rules
    
    * feat(bst_main): code for getting the inorder traversal
    
    * docs(bst_main, bst_pseudocode): fixed pseudocode and some comments

---
+++ binary_search_tree_application/bst_main.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+import random
+
+class Node:
+    def __init__(self, data):
+        self.n_left = None
+        self.n_val = data
+        self.n_right = None
+
+class BSTree:
+    def __init__(self, countnode):
+        if not isinstance(countnode, int):
+            raise TypeError("Enter a valid number between 10 to 30 only")
+        if countnode < 10 or countnode > 30:
+            raise ValueError("Enter a valid number between 10 to 30 only")
+        
+        self.countnode = countnode
+    
+    def create_node(self, value):
+        return Node(value)
+    
+    def node_child(self, node, value):
+        if node == None:
+            return self.create_node(value)
+        
+        if value <= node.n_val:                                    # duplicates will automatically be on the left child (as per sir)
+            node.n_left = self.node_child(node.n_left, value)
+        else:
+            node.n_right = self.node_child(node.n_right, value)
+        
+        return node
+    
+    # LTR (inorder) traversal
+    def inorder(self, root):
+        if root is not None:
+            self.inorder(root.n_left)
+            print(root.n_val)
+            self.inorder(root.n_right)
+
+class BSTMaker:
+    pass
+
+# if __name__ == "__main__":
