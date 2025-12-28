9cd91b3c907a6bf0aa3d6f7fe46f3fb146226f58
Kristina Cas <m.kristinacas@gmail.com>
2025-12-28 16:55:13 +0800

    feat(main_app): selection page bg and buttons

--- main_app.py
+++ main_app.py
@@ -76,13 +76,126 @@ class StartPage(tk.Frame):
         
     def resize_bg(self, event):
         resized = self.bg_img.resize((event.width, event.height))
-        self.start_bg = ImageTk.PhotoImage(resized)
+        self.select_bg = ImageTk.PhotoImage(resized)
         
         self.canvas.itemconfig(self.canvas_bg, image=self.start_bg)
     
 
 class SelectPage(tk.Frame):
-    pass
+    def __init__(self, parent, controller):
+        super().__init__(parent)                 # will be called to parent class
+        
+        ''' FOR BACKGROUND SETUP '''
+        # canvas for storing the background of start page
+        self.canvas = tk.Canvas(self)
+        self.canvas.pack(fill="both", expand=True)
+        
+        # load the background image in canvas
+        self.bg_img = Image.open("assets/select_bg.png")
+        self.select_bg = ImageTk.PhotoImage(self.bg_img)
+        
+        # draw bg  
+        self.canvas_bg = self.canvas.create_image(0, 0, image=self.select_bg, anchor="nw")
+        
+        # auto resize of background
+        self.canvas.bind("<Configure>", self.resize_bg)  
+        
+        ''' FOR BUTTONS AND LABELS '''
+        # home button to go back to home
+        home_button = tk.Button(
+            self,
+            text="HOME",
+            font=("VT323", 12),
+            bg = "#594faf",
+            fg = "#ffffff",
+            activebackground="#330084",
+            activeforeground="#ffffff",
+            padx=65,
+            command=lambda: controller.show_frame("StartPage")
+        )
+        
+        self.canvas.create_window(103, 38, window=home_button) 
+        
+        # stack button to direct to stack garage page
+        stack_button = tk.Button(
+            self,
+            text="STACK",
+            font=("VT323", 20),
+            bg = "#ecb1ff",
+            fg = "#330084",
+            activebackground="#330084",
+            activeforeground="#ffffff",
+            padx=73,
+            command=lambda: controller.show_frame("StackPage")
+        )
+        
+        self.canvas.create_window(300, 400, window=stack_button)
+        
+        # queue button to direct to queue garage page
+        queue_button = tk.Button(
+            self,
+            text="QUEUE",
+            font=("VT323", 20),
+            bg = "#ecb1ff",
+            fg = "#330084",
+            activebackground="#330084",
+            activeforeground="#ffffff",
+            padx=70,
+            command=lambda: controller.show_frame("QueuePage")
+        )
+        
+        self.canvas.create_window(1250, 400, window=queue_button)
+        
+        # bt button to direct to binary tree page
+        bt_button = tk.Button(
+            self,
+            text="BINARY TREE",
+            font=("VT323", 20),
+            bg = "#ecb1ff",
+            fg = "#330084",
+            activebackground="#330084",
+            activeforeground="#ffffff",
+            padx=60,
+            command=lambda: controller.show_frame("BTPage")
+        )
+        
+        self.canvas.create_window(773, 520, window=bt_button)
+        
+        # bst button to direct to binary search tree page
+        bst_button = tk.Button(
+            self,
+            text="BINARY SEARCH TREE",
+            font=("VT323", 20),
+            bg = "#ecb1ff",
+            fg = "#330084",
+            activebackground="#330084",
+            activeforeground="#ffffff",
+            padx=3,
+            command=lambda: controller.show_frame("BSTPage")
+        )
+        
+        self.canvas.create_window(300, 650, window=bst_button)
+        
+        # recursion button to direct to hanoi page
+        hanoi_button = tk.Button(
+            self,
+            text="RECURSION",
+            font=("VT323", 20),
+            bg = "#ecb1ff",
+            fg = "#330084",
+            activebackground="#330084",
+            activeforeground="#ffffff",
+            padx=50,
+            command=lambda: controller.show_frame("HanoiPage")
+        )
+        
+        self.canvas.create_window(1250, 646, window=hanoi_button)
+        
+    def resize_bg(self, event):
+        resized = self.bg_img.resize((event.width, event.height))
+        self.select_bg = ImageTk.PhotoImage(resized)
+        
+        self.canvas.itemconfig(self.canvas_bg, image=self.select_bg)
 
 class StackPage(tk.Frame):
     pass
