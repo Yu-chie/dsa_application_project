21e4fc6b719b27c5bf15dfa029e9a32c8ca0b146
Kristina Cas <m.kristinacas@gmail.com>
2025-12-28 16:29:59 +0800

    feat(main_app): initialized classes and import of librarie

---
+++ main_app.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+
+class MainApp(tk.Tk):
+    pass
+
+class StartPage(tk.Frame):
+    pass
+
+class SelectPage(tk.Frame):
+    pass
+
+class StackPage(tk.Frame):
+    pass
+
+class QueuePage(tk.Frame):
+    pass
+
+class BTPage(tk.Frame):
+    pass
+
+class BSTPage(tk.Frame):
+    pass
+
+class DevPage(tk.Frame):
+    pass
+
+
+if __name__ == "__main__":
+    run_app = MainApp()
+    run_app.mainloop()
