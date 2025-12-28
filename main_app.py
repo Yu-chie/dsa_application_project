0990d50cbc1a93501b65f66bc3dd52c3c4929e40
Kristina Cas <m.kristinacas@gmail.com>
2025-12-28 16:36:30 +0800

    feat(main_app): main app class for general switch of pages

--- main_app.py
+++ main_app.py
@@ -2,7 +2,29 @@ import tkinter as tk
 from PIL import Image, ImageTk
 
 class MainApp(tk.Tk):
-    pass
+    def __init__(self):
+        super().__init()       # initialize tk and set this as parent class
+        
+        # general window title, dimension, and allow fullscreen
+        self.title("DSA App")
+        self.geometry("1920x1080")
+        self.resizable(True, True)
+        
+        # for holding pages as frames (allow smooth change of windows)
+        container = tk.Frame(self)
+        container.pack(fill="both", expand=True)
+        
+        self.frames = {}
+        # list of pages included (so if may dinedevelop na page i-add ung class dito para magpakita pag ni-run)
+        for page in (StartPage, SelectPage, DevPage):  
+            frame = page(container, self)
+            self.frames[page.__name__] = frame
+            frame.place(relwidth=1, relheight=1)
+            
+        self.show_frame("StartPage")   # first page shown will be the Start Page
+    
+    def show_frame(self, page_name):
+        self.frames[page_name].tkraise()
 
 class StartPage(tk.Frame):
     pass
