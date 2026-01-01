a3deeee6ba6a10377dc99d6a40d00d2c7bd97242
Kristina Cas <m.kristinacas@gmail.com>
2026-01-01 18:54:33 +0800

    perf(bt_main): remove error checking in Tree class as it was moved to the gui

--- binary_tree_application/bt_main.py
+++ binary_tree_application/bt_main.py
@@ -10,11 +10,6 @@ class Node:
 # class to make the tree - connections of nodes
 class Tree:
     def __init__(self, levels):
-        if not isinstance(levels, int):                                          # checks if user input is correct
-            raise TypeError("Enter a valid number between 1 to 5 only")
-        if levels < 1 or levels > 5:
-            raise ValueError("Enter a valid number between 1 to 5 only")
-        
         self.levels = levels                                                     # will get value if correct input
         
     # build tree
