1c3b94a67e4fb7343d2b9a277118b3c41767286f
Kristina Cas <m.kristinacas@gmail.com>
2026-01-01 18:47:07 +0800

    feat(bt_gui): logic holders

--- binary_tree_application/bt_gui.py
+++ binary_tree_application/bt_gui.py
@@ -38,6 +38,13 @@ class BTPage(tk.Frame):
         
         self.canvas.create_window(103, 45, window=home_button)
         
+        '''  LOGIC HOLDERS  '''
+        self.tree = None
+        self.n_root = None
+        self.values = []
+        
+        self.treelvl_input()
+        
     def resize_bg(self, event):
         resized = self.bg_img.resize((event.width, event.height))
         self.btree_bg = ImageTk.PhotoImage(resized)
