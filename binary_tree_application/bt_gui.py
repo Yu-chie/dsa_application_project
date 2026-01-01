3ad5c0c9ae180db5a044c9d33438cf5715f806bd
Kristina Cas <m.kristinacas@gmail.com>
2026-01-01 18:46:33 +0800

    feat(bt_gui): base methods for background generation

--- binary_tree_application/bt_gui.py
+++ binary_tree_application/bt_gui.py
@@ -2,3 +2,44 @@ import tkinter as tk
 from tkinter import messagebox
 from PIL import Image, ImageTk
 from bt_main import Tree
+
+class BTPage(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+        
+        # canvas for binary tree bg 
+        self.canvas = tk.Canvas(self)
+        self.canvas.pack(fill="both", expand=True)
+        
+        # load bg image
+        self.bg_img = Image.open("pics/bt_bg.png")
+        self.btree_bg = ImageTk.PhotoImage(self.bg_img)
+        
+        # draw bg 
+        self.canvas_bg = self.canvas.create_image(0, 0, image=self.btree_bg, anchor="nw")
+        
+        # auto resizing bg 
+        self.canvas.bind("<Configure>", self.resize_bg)
+        
+        # to home button
+        home_button = tk.Button(
+            self,
+            text="HOME",
+            font=("VT323", 12),
+            bg = "#594faf",
+            fg = "#ffffff",
+            activebackground="#330084",
+            activeforeground="#ffffff",
+            padx=65,
+            pady=7,
+            command=lambda: controller.show_frame("StartPage")
+        )
+        
+        self.canvas.create_window(103, 45, window=home_button)
+        
+    def resize_bg(self, event):
+        resized = self.bg_img.resize((event.width, event.height))
+        self.btree_bg = ImageTk.PhotoImage(resized)
+        
+        self.canvas.itemconfig(self.canvas_bg, image=self.btree_bg)
