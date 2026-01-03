77158af8efb281c00190bb10d4932990a3064c95
Kristina Cas <m.kristinacas@gmail.com>
2026-01-03 22:08:39 +0800

    feat(bst_gui) setup background of bst page

--- binary_search_tree_application/bst_gui.py
+++ binary_search_tree_application/bst_gui.py
@@ -6,10 +6,44 @@ import random
 
 class BSTPage(tk.Frame):
     def __init__(self, parent, controller):
-        pass
+        super().__init__(parent)
+        self.controller = controller
+        
+        # canvas for binary tree bg 
+        self.canvas = tk.Canvas(self)
+        self.canvas.pack(fill="both", expand=True)
+        
+        # load bg image
+        self.bg_img = Image.open("assets/bst_bg.png")
+        self.bstree_bg = ImageTk.PhotoImage(self.bg_img)
+        
+        # draw bg 
+        self.canvas_bg = self.canvas.create_image(0, 0, image=self.bstree_bg, anchor="nw")
+        
+        # auto resizing bg 
+        self.canvas.bind("<Configure>", self.resize_bg)
+        
+        # to home button
+        home_button = tk.Button(
+            self,
+            text="HOME",
+            font=("VT323", 12),
+            bg = "#594faf",
+            fg = "#ffffff",
+            activebackground="#330084",
+            activeforeground="#ffffff",
+            padx=65,
+            pady=7,
+            command=lambda: controller.show_frame("StartPage")
+        )
+        
+        self.canvas.create_window(103, 45, window=home_button)
     
     def resize_bg(self, event):
-        pass
+        resized = self.bg_img.resize((event.width, event.height))
+        self.btree_bg = ImageTk.PhotoImage(resized)
+        
+        self.canvas.itemconfig(self.canvas_bg, image=self.btree_bg)
     
     ''' BST METHOD CHOICE '''
     def user_decision(self):
