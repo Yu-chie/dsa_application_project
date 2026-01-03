552a85036b2902430467dad8075b6a01347a0843
Kristina Cas <m.kristinacas@gmail.com>
2026-01-03 22:01:31 +0800

    feat(bst_gui): added class outline

---
+++ binary_search_tree_application/bst_gui.py
@@ -0,0 +1,47 @@
+import tkinter as tk
+from tkinter import messagebox
+from PIL import Image, ImageTk
+from binary_search_tree_application.bst_main import BSTree
+import random
+
+class BSTPage(tk.Frame):
+    def __init__(self, parent, controller):
+        pass
+    
+    def resize_bg(self, event):
+        pass
+    
+    ''' BST METHOD CHOICE '''
+    def user_decision(self):
+        pass
+    
+    ''' BST METHOD CONFIRMATION '''
+    def confirm_udecision(self):
+        pass
+    
+    ''' FOR NODE COUNT INPUT '''
+    def nodecount_input(self):
+        pass
+    
+    ''' NODE COUNT CONFIRMATION '''
+    def confirm_nodecount(self):
+        pass
+    
+    ''' USER PROVIDED VALUES FOR BST BUTTONS '''
+    def node_user(self):
+        pass
+    
+    ''' DRAWING BSTREE '''
+    def generate_bstree(self):
+        pass
+    
+    ''' FOR TRAVERSAL TITLE'''
+    def traversal_title(self):
+        pass
+    
+    ''' FOR PRINTING TRAVERSAL '''
+    def traversal_holder(self):
+        pass
+    
+    def reset_tree(self):
+        pass
