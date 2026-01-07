a8b7075d42ea1c0d0de45c583896ed0918f4e2a5
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2026-01-07 14:17:27 +0800

    refactor(queue_gui.py): store main controller reference

--- main_app.py
+++ main_app.py
@@ -220,7 +220,8 @@ class StackPage(tk.Frame):
     pass
 
 class QueuePage(tk.Frame):
-    def __init__(self, parent, controller):
+    def __init__(self, parent, garage, controller):
+        self.controller = controller
         super().__init__(parent)
         
         # File Manager setup
