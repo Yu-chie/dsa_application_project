2cc875ff3ae35b632c68bb11f38d198d63dbbd37
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2026-01-16 18:39:46 +0800

    fix(queue_gui): rename start_application and stop_application to resolve AttributionError

--- queue_application/queue_gui.py
+++ queue_application/queue_gui.py
@@ -139,7 +139,7 @@ class QueueGUI(tk.Frame):
             text="START",
             font=("VT323", 14),
             width=15,
-            command=self.controller.start_application
+            command=self.controller.start_simulation
         )
         
         self.start_btn_window = self.canvas.create_window(
@@ -152,7 +152,7 @@ class QueueGUI(tk.Frame):
             text="STOP",
             font=("VT323", 14),
             width=15,
-            command=self.controller.stop_application
+            command=self.controller.stop_simulation
         )
 
         self.stop_btn_window = self.canvas.create_window(
